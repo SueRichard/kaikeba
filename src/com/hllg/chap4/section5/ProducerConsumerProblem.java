6a26c176e4a09dabe31d38cb8213538bf7ef9c60
SueRichard <hanleideying@126.com>
2021-02-04 16:44:19 +0800

    用于测试生产者消费者问题 追加synchronized关键字的效果，还不能解决问题， 2021年2月4日16:44:14

--- src/com/hllg/chap4/section5/ProducerConsumerProblem.java
+++ src/com/hllg/chap4/section5/ProducerConsumerProblem.java
@@ -4,6 +4,7 @@ package com.hllg.chap4.section5;
  * @author HLLG
  * @version 1.0
  * @create 04/02/2021  16:13
+ * 假设一个碟子，一个厨师，一个服务员，做完菜端出等碟子回来再继续做
  */
 public class ProducerConsumerProblem {
     public static void main(String[] args) {
@@ -58,22 +59,31 @@ public class ProducerConsumerProblem {
         }
     }
 
+    /**
+     * 菜
+     */
     static class Food {
         private String name;
         private String taste;
 
-        public void setNameAndTaste(String name, String taste) {
+        /**
+         * 对改方法追加同步，但却不能解决问题
+         * 因为synchronized是非公平锁，当你厨师做完，并不一定能给到服务员线程，
+         * 就导致厨师做了多个菜，但是没来得及端出，然后最后厨师先做完了，服务员在重复端厨师做的最后一个菜
+         */
+        public synchronized void setNameAndTaste(String name, String taste) {
             this.name = name;
             try {
                 //制造时间片隙
                 Thread.sleep(100);
+                //菜名改了，但是味道没有改，被服务员线程抢占了
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             this.taste = taste;
         }
 
-        public void take() {
+        public synchronized void take() {
             System.out.println("服务员端走的菜是：" + name + "，味道：" + taste);
         }
     }
